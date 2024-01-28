de09e0e2151fceabc101aa5bba75990b98a48978
LinCanhui <lingyun2311@gmail.com>
2024-01-29 01:33:10 +0800

    连接 MySQL 数据库，并对其进行测试

--- src/test/java/com/mlinyun/usercenter/UserCenterApplicationTests.java
+++ src/test/java/com/mlinyun/usercenter/UserCenterApplicationTests.java
@@ -1,13 +1,33 @@
 package com.mlinyun.usercenter;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class UserCenterApplicationTests {
 
+    // 注入数据源对象
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void datasourceTest() throws SQLException {
+        // 获取数据源类型
+        System.out.println("默认数据源为：" + dataSource.getClass());
+        // 获取数据库连接对象
+        Connection connection = dataSource.getConnection();
+        // 判断连接对象是否为空
+        System.out.println(connection != null);
+        connection.close();
     }
 
 }
