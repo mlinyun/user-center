20d6eb76aa89bfd8774f8561260ccf5119df33ef
LinCanhui <lingyun2311@gmail.com>
2024-08-12 17:49:00 +0800

    连接 MySQL 数据库，并对其进行测试

---
+++ sql/user-center-back.sql
@@ -0,0 +1,53 @@
+-- 创建数据库
+DROP DATABASE IF EXISTS `user_center`;
+CREATE DATABASE IF NOT EXISTS `user_center` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT ENCRYPTION = 'N';
+USE `user_center`;
+
+-- MyBatis Plus 测试用到的表和数据（开始）
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`
+(
+    id    BIGINT      NOT NULL COMMENT '主键ID',
+    name  VARCHAR(30) NULL DEFAULT NULL COMMENT '姓名',
+    age   INT         NULL DEFAULT NULL COMMENT '年龄',
+    email VARCHAR(50) NULL DEFAULT NULL COMMENT '邮箱',
+    PRIMARY KEY (id)
+);
+
+DELETE
+FROM `user`;
+
+INSERT INTO `user` (id, name, age, email)
+VALUES (1, 'Jone', 18, 'test1@baomidou.com'),
+       (2, 'Jack', 20, 'test2@baomidou.com'),
+       (3, 'Tom', 28, 'test3@baomidou.com'),
+       (4, 'Sandy', 21, 'test4@baomidou.com'),
+       (5, 'Billie', 24, 'test5@baomidou.com');
+-- MyBatis Plus 测试用到的表和数据（结束）
+
+-- 项目正式用到的用户表
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE user_center.user
+(
+    id           bigint auto_increment comment 'id' primary key,
+    username     varchar(256)                       null comment '用户昵称',
+    userAccount  varchar(256)                       null comment '用户账号',
+    avatarUrl    varchar(1024)                      null comment '用户头像',
+    gender       tinyint                            null comment '性别',
+    userPassword varchar(512)                       not null comment '用户密码',
+    phone        varchar(128)                       null comment '电话',
+    email        varchar(512)                       null comment '邮箱',
+    userStatus   int      default 0                 not null comment '用户状态 0 - 正常',
+    createTime   datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint  default 0                 not null comment '是否删除',
+    userRole     int      default 0                 not null comment '用户角色 0 - 普通用户 1 - 管理员',
+    planetCode   varchar(512)                       not null comment '星球编号'
+) ENGINE = InnoBD
+  DEFAULT CHARSET = utf8mb4;
+
+INSERT INTO `user`
+VALUES (1, '凌云', 'LingYunAdmin', '/avatar.png', 1, '596b8d3f950830fda18e7ef035bd0918', '15600000000',
+        '1234567890@qq.com', 0, '2024-01-30 01:01:10', '2024-02-05 01:57:05', 0, 1, '1'),
+       (2, '凌云2', 'LingYunUser', '/avatar.png', 0, '596b8d3f950830fda18e7ef035bd0918', '15611111111',
+        '1000000000@qq.com', 0, '2024-01-30 01:01:10', '2024-02-05 01:57:05', 0, 0, '2');
--- src/test/java/com/mlinyun/usercenterback/UserCenterBackApplicationTests.java
+++ src/test/java/com/mlinyun/usercenterback/UserCenterBackApplicationTests.java
@@ -1,13 +1,36 @@
 package com.mlinyun.usercenterback;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class UserCenterBackApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    // 注入数据源对象
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void dataSourceTest() throws SQLException {
+        // 获取数据源类型
+        System.out.println("默认数据源为：" + dataSource.getClass());
+        // 获取数据库连接对象
+        Connection connection = dataSource.getConnection();
+        // 判断连接对象是否为空（若不为空（True）表示数据库连接成功）
+        System.out.println(connection != null);
+        // 关闭连接对象
+        if (connection != null) {
+            connection.close();
+        }
     }
 
 }
