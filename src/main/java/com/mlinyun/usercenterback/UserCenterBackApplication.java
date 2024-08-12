2e61494613bb9171b4440233f2321d6cb8b9d025
LinCanhui <lingyun2311@gmail.com>
2024-08-12 15:37:42 +0800

    初始化后端项目

---
+++ src/main/java/com/mlinyun/usercenterback/UserCenterBackApplication.java
@@ -0,0 +1,13 @@
+package com.mlinyun.usercenterback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCenterBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterBackApplication.class, args);
+    }
+
+}
